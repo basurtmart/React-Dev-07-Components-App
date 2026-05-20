b2a0bbe00c095388b51e026eccf0453e4ce0a300
mbasurto <mbd.97@hotmail.com>
2026-05-19 21:26:35 -0600

    feat: Refactor AlertsScreen to use ThemedButton and ThemedView for improved UI

--- app/alerts/index.tsx
+++ app/alerts/index.tsx
@@ -1,10 +1,57 @@
-import { View, Text } from 'react-native';
+import ThemedButton from '@/presentation/shared/ThemedButton';
+import ThemedView from '@/presentation/shared/ThemedView';
+import { Alert, StyleSheet } from 'react-native';
 
 const AlertsScreen = () => {
+  const createTwoButtonAlert = () =>
+    Alert.alert('Alert Title', 'My Alert Msg', [{
+      text: 'Cancel',
+      onPress: () => console.log('Cancel Pressed'),
+      style: 'cancel',
+    }, {
+      text: 'Ok',
+      onPress: () => console.log('Ok Pressed')
+    },
+    ]);
+
+  const createThreeButtonAlert = () =>
+    Alert.alert('Alert Title', 'My Alert Msg', [{
+      text: 'Ask me later',
+      onPress: () => console.log('Ask me later pressed'),
+    }, {
+      text: 'Cancel',
+      onPress: () => console.log('Cancel Pressed'),
+      style: 'cancel',
+    }, {
+      text: 'Ok',
+      onPress: () => console.log('Ok Pressed')
+    },
+    ]);
+
+
   return (
-    <View>
-      <Text>AlertsScreen</Text>
-    </View>
+    <ThemedView margin>
+      <ThemedButton
+        className='my-5'
+        onPress={createTwoButtonAlert}>
+        2-Button Alert
+      </ThemedButton>
+
+      <ThemedButton
+        className='mb-5'
+        onPress={createThreeButtonAlert}>
+        3-Button Alert
+      </ThemedButton>
+    </ThemedView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+});
+
 export default AlertsScreen;

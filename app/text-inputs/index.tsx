2ffdb6a25b3f9c6f28a2daec6676f74c2ddb5c1a
mbasurto <mbd.97@hotmail.com>
2026-05-19 22:33:04 -0600

    feat: Implement TextInputsScreen with ThemedTextInput for improved user input handling

--- app/text-inputs/index.tsx
+++ app/text-inputs/index.tsx
@@ -1,10 +1,48 @@
-import { View, Text } from 'react-native';
+import ThemedCard from '@/presentation/shared/ThemedCard';
+import ThemedText from '@/presentation/shared/ThemedText';
+import ThemedTextInput from '@/presentation/shared/ThemedTextInput';
+import ThemedView from '@/presentation/shared/ThemedView';
+import { useState } from 'react';
 
 const TextInputsScreen = () => {
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    phone: '',
+  });
+
   return (
-    <View>
-      <Text>TextInputsScreen</Text>
-    </View>
+    <ThemedView margin>
+      <ThemedCard className='mb-5'>
+        <ThemedTextInput
+          placeholder='Nombre completo'
+          autoCapitalize={'words'}
+          autoCorrect={false}
+          onChangeText={(text) => setForm({ ...form, name: text })} />
+      </ThemedCard>
+
+      <ThemedCard className='mb-5'>
+        <ThemedTextInput
+          placeholder='Correo electronico'
+          autoCapitalize={'none'}
+          autoCorrect={false}
+          keyboardType='email-address'
+          onChangeText={(text) => setForm({ ...form, email: text })} />
+      </ThemedCard>
+
+      <ThemedCard className='mb-5'>
+        <ThemedTextInput
+          placeholder='Telefono'
+          autoCapitalize={'none'}
+          autoCorrect={false}
+          keyboardType='phone-pad'
+          onChangeText={(text) => setForm({ ...form, phone: text })} />
+      </ThemedCard>
+
+      <ThemedCard>
+        <ThemedText>{JSON.stringify(form, null, 2)}</ThemedText>
+      </ThemedCard>
+    </ThemedView>
   );
 };
 export default TextInputsScreen;

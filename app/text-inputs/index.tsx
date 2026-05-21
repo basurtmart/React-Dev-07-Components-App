fb1005a85d0195b51260ced93ce284278993ba60
mbasurto <mbd.97@hotmail.com>
2026-05-20 19:19:40 -0600

    feat: Enhance TextInputsScreen layout with KeyboardAvoidingView and ScrollView for better user experience

--- app/text-inputs/index.tsx
+++ app/text-inputs/index.tsx
@@ -3,6 +3,9 @@ import ThemedText from '@/presentation/shared/ThemedText';
 import ThemedTextInput from '@/presentation/shared/ThemedTextInput';
 import ThemedView from '@/presentation/shared/ThemedView';
 import { useState } from 'react';
+import { KeyboardAvoidingView, Platform, ScrollView } from 'react-native';
+
+const isIOS = Platform.OS === 'ios';
 
 const TextInputsScreen = () => {
   const [form, setForm] = useState({
@@ -12,37 +15,73 @@ const TextInputsScreen = () => {
   });
 
   return (
-    <ThemedView margin>
-      <ThemedCard className='mb-5'>
-        <ThemedTextInput
-          placeholder='Nombre completo'
-          autoCapitalize={'words'}
-          autoCorrect={false}
-          onChangeText={(text) => setForm({ ...form, name: text })} />
-      </ThemedCard>
+    <KeyboardAvoidingView
+      behavior={'height'}>
+      <ScrollView>
+        <ThemedView margin>
+          <ThemedCard className='mb-5'>
+            <ThemedTextInput
+              placeholder='Nombre completo'
+              autoCapitalize={'words'}
+              autoCorrect={false}
+              onChangeText={(text) => setForm({ ...form, name: text })} />
+          </ThemedCard>
 
-      <ThemedCard className='mb-5'>
-        <ThemedTextInput
-          placeholder='Correo electronico'
-          autoCapitalize={'none'}
-          autoCorrect={false}
-          keyboardType='email-address'
-          onChangeText={(text) => setForm({ ...form, email: text })} />
-      </ThemedCard>
+          <ThemedCard className='mb-5'>
+            <ThemedTextInput
+              placeholder='Correo electrónico'
+              autoCorrect={false}
+              keyboardType='email-address'
+              onChangeText={(text) => setForm({ ...form, email: text })} />
+          </ThemedCard>
 
-      <ThemedCard className='mb-5'>
-        <ThemedTextInput
-          placeholder='Telefono'
-          autoCapitalize={'none'}
-          autoCorrect={false}
-          keyboardType='phone-pad'
-          onChangeText={(text) => setForm({ ...form, phone: text })} />
-      </ThemedCard>
+          <ThemedCard className='mb-5'>
+            <ThemedTextInput
+              placeholder='Teléfono'
+              autoCapitalize={'none'}
+              autoCorrect={false}
+              keyboardType='phone-pad'
+              onChangeText={(text) => setForm({ ...form, phone: text })} />
+          </ThemedCard>
 
-      <ThemedCard>
-        <ThemedText>{JSON.stringify(form, null, 2)}</ThemedText>
-      </ThemedCard>
-    </ThemedView>
+          <ThemedCard className='my-2'>
+            <ThemedText>{JSON.stringify(form, null, 2)}</ThemedText>
+          </ThemedCard>
+
+          <ThemedCard className='my-2'>
+            <ThemedText>{JSON.stringify(form, null, 2)}</ThemedText>
+          </ThemedCard>
+
+          <ThemedCard className='my-2'>
+            <ThemedText>{JSON.stringify(form, null, 2)}</ThemedText>
+          </ThemedCard>
+
+          <ThemedCard className='my-2'>
+            <ThemedText>{JSON.stringify(form, null, 2)}</ThemedText>
+          </ThemedCard>
+
+          <ThemedCard className='my-2'>
+            <ThemedText>{JSON.stringify(form, null, 2)}</ThemedText>
+          </ThemedCard>
+
+          <ThemedCard className='my-2'>
+            <ThemedText>{JSON.stringify(form, null, 2)}</ThemedText>
+          </ThemedCard>
+
+          <ThemedCard
+            style={{
+              marginBottom: isIOS ? 120 : 100,
+            }}>
+            <ThemedTextInput
+              placeholder='Teléfono'
+              autoCapitalize={'none'}
+              autoCorrect={false}
+              keyboardType='phone-pad'
+              onChangeText={(text) => setForm({ ...form, phone: text })} />
+          </ThemedCard>
+        </ThemedView>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 export default TextInputsScreen;
